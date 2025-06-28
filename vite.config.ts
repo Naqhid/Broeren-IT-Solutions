5a944155db1dc4701b227c250df8395ee37fcf0a
Mohammed Naqhid <mnaqhid@gmail.com>
2025-06-28 11:06:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Broeren-IT-Solutions/', // <-- Set repo name here
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
